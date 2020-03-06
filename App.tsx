b9af53be4943ed1cede36b4c021abc44e9834a00
Rhys Mitchell <rhys.mitchell92@googlemail.com>
2020-03-06 08:52:02 +0000

    Remove HML.. hot reloading pages seems to work without it automagically.

--- App.tsx
+++ App.tsx
@@ -1,7 +1,5 @@
-import React, { FunctionComponent } from "react";
-import { hot } from "react-hot-loader";
+import React, { FunctionComponent } from 'react';
 
 const App: FunctionComponent = (): JSX.Element => <h1>Hello world!</h1>;
 
-declare const module: any;
-export default hot(module)(App);
+export default App;
